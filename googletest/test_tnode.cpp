373ddfc6925cadc4fda7ec7c91e10693b387b8c7
Sam Miller <millerss@Sams-MacBook-Air.local>
2024-11-02 11:16:10 -0400

    TNode tests.

--- googletest/test_tnode.cpp
+++ googletest/test_tnode.cpp
@@ -1,13 +1,11 @@
 #include "../src/btree.h"
-
-#include "../src/btree.h"
 #include "../src/key.h"
 #include "../src/node.h"
 #include <gtest/gtest.h>
 
 
 
-TEST(TNodeTest, TestCreateNode) {
+TEST(TNodeTest, Constructor) {
   NodeNbr m_nbr(1);
   std::string m_filename = "tnodetest";
   IndexFile m_idxfile(m_filename);
@@ -20,7 +18,7 @@ TEST(TNodeTest, TestCreateNode) {
   remove((m_filename+".ndx").c_str());
 }
 
-TEST(TNodeTest, TestInsert) {
+TEST(TNodeTest, Insert) {
   NodeNbr m_nbr(1);
   std::string m_filename = "tnodetest";
   IndexFile m_idxfile(m_filename);
@@ -36,7 +34,7 @@ TEST(TNodeTest, TestInsert) {
   remove((m_filename+".ndx").c_str());
 }
 
-TEST(TNodeTest, TestInsertAndSearch) {
+TEST(TNodeTest, InsertAndSearch) {
   NodeNbr m_nbr(1);
   std::string m_filename = "tnodetest";
   IndexFile m_idxfile(m_filename);
@@ -70,3 +68,103 @@ TEST(TNodeTest, IsLeafInitiallyFalse) {
 }
 
 
+TEST(TNodeTest, WriteBtreeKeyIsLeafFalse) {
+  NodeNbr m_nbr(1);
+  std::string m_filename = "tnodewritebtreetest";
+  IndexFile m_idxfile(m_filename);
+  Class m_class{};
+  int i = 1;
+  Key<int> m_key (i);
+  PdyBtree btree(m_idxfile, &m_class, &m_key);
+  TNode m_tnode(&btree, m_nbr);
+  EXPECT_EQ(m_tnode.header.keycount, 0);
+  Key<int> write_key(2);
+  m_tnode.WriteBtreeKey(&write_key);
+  // TODO: What can we test here?
+  // Where is the data being written and what should it have?
+  remove((m_filename+".ndx").c_str());
+}
+
+TEST(TNodeTest, WriteBtreeKeyIsLeafTrue) {
+  NodeNbr m_nbr(1);
+  std::string m_filename = "tnodewritebtreetest";
+  IndexFile m_idxfile(m_filename);
+  Class m_class{};
+  int i = 1;
+  Key<int> m_key (i);
+  PdyBtree btree(m_idxfile, &m_class, &m_key);
+  TNode m_tnode(&btree, m_nbr);
+  EXPECT_EQ(m_tnode.header.keycount, 0);
+  m_tnode.header.isleaf = true;
+  Key<int> write_key(2);
+  m_tnode.WriteBtreeKey(&write_key);
+  // TODO: What can we test here?
+  // Where is the data being written and what should it have?
+  remove((m_filename+".ndx").c_str());
+}
+
+
+TEST(TNodeTest, Deconstructor) {
+  NodeNbr m_nbr(1);
+  std::string m_filename = "tnodetest";
+  IndexFile m_idxfile(m_filename);
+  Class m_class{};
+  int i = 1;
+  Key<int> m_key (i);
+  PdyBtree btree(m_idxfile, &m_class, &m_key);
+  auto m_tnode = new TNode(&btree, m_nbr);
+  delete m_tnode;
+  remove((m_filename+".ndx").c_str());
+}
+
+
+TEST(TNodeTest, DeconstructorNodechanged) {
+  NodeNbr m_nbr(1);
+  std::string m_filename = "tnodetest";
+  IndexFile m_idxfile(m_filename);
+  Class m_class{};
+  int i = 1;
+  Key<int> m_key (i);
+  PdyBtree btree(m_idxfile, &m_class, &m_key);
+  auto m_tnode = new TNode(&btree, m_nbr);
+  m_tnode->nodechanged = true;
+  delete m_tnode;
+  remove((m_filename+".ndx").c_str());
+}
+
+
+TEST(TNodeTest, DeconstructorManyKeys) {
+  NodeNbr m_nbr(1);
+  std::string m_filename = "tnodetest";
+  IndexFile m_idxfile(m_filename);
+  Class m_class{};
+  int i = 1;
+  Key<int> m_key (i);
+  PdyBtree btree(m_idxfile, &m_class, &m_key);
+  auto m_tnode = new TNode(&btree, m_nbr);
+  Key<int> insert_key1(2);
+  Key<int> insert_key2(4);
+  m_tnode->Insert(&insert_key1);
+  m_tnode->Insert(&insert_key2);
+  m_tnode->nodechanged = false;
+  delete m_tnode;
+  remove((m_filename+".ndx").c_str());
+}
+
+TEST(TNodeTest, DeconstructorManyKeysNodeChanged) {
+  NodeNbr m_nbr(1);
+  std::string m_filename = "tnodetest";
+  IndexFile m_idxfile(m_filename);
+  Class m_class{};
+  int i = 1;
+  Key<int> m_key (i);
+  PdyBtree btree(m_idxfile, &m_class, &m_key);
+  auto m_tnode = new TNode(&btree, m_nbr);
+  Key<int> insert_key1(2);
+  Key<int> insert_key2(4);
+  m_tnode->Insert(&insert_key1);
+  m_tnode->Insert(&insert_key2);
+  m_tnode->nodechanged = true;
+  delete m_tnode;
+  remove((m_filename+".ndx").c_str());
+}
