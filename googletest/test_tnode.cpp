ecb36ccb4c82fe68f305be57597ddd8336856dd7
Sam Miller <millerss@Sams-MacBook-Air.local>
2024-11-02 11:05:44 -0400

    TNode tests.

--- googletest/test_tnode.cpp
+++ googletest/test_tnode.cpp
@@ -35,3 +35,38 @@ TEST(TNodeTest, TestInsert) {
   EXPECT_EQ(m_tnode.header.keycount, 1);
   remove((m_filename+".ndx").c_str());
 }
+
+TEST(TNodeTest, TestInsertAndSearch) {
+  NodeNbr m_nbr(1);
+  std::string m_filename = "tnodetest";
+  IndexFile m_idxfile(m_filename);
+  Class m_class{};
+  int i = 1;
+  Key<int> m_key (i);
+  PdyBtree btree(m_idxfile, &m_class, &m_key);
+  TNode m_tnode(&btree, m_nbr);
+  EXPECT_EQ(m_tnode.header.keycount, 0);
+  Key<int> insert_key1(2);
+  Key<int> insert_key2(4);
+  m_tnode.Insert(&insert_key1);
+  m_tnode.Insert(&insert_key2);
+  EXPECT_EQ(m_tnode.header.keycount, 2);
+  auto result = m_tnode.SearchNode(&insert_key2);
+  EXPECT_TRUE(result);
+  remove((m_filename+".ndx").c_str());
+}
+
+TEST(TNodeTest, IsLeafInitiallyFalse) {
+  NodeNbr m_nbr(1);
+  std::string m_filename = "tnodetest";
+  IndexFile m_idxfile(m_filename);
+  Class m_class{};
+  int i = 1;
+  Key<int> m_key (i);
+  PdyBtree btree(m_idxfile, &m_class, &m_key);
+  TNode m_tnode(&btree, m_nbr);
+  EXPECT_EQ(m_tnode.isLeaf(), false);
+  remove((m_filename+".ndx").c_str());
+}
+
+
